9aae2998ae5bc3c014d4b72e7f8d6d2609d30492
Dimitrios Traskas <dtraskas@gmail.com>
2017-01-11 16:33:07 +0000

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker build -t gcr.io/caci-main/hdfs-spark-base spark-base && \
+docker build -t gcr.io/caci-main/spark-master spark-master && \
+docker build -t gcr.io/caci-main/spark-worker spark-worker
