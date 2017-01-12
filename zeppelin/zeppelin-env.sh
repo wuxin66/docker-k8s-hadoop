f574450d25eb973016eb299b0c45718eec603a97
dtraskas <dtraskas@gmail.com>
2017-01-12 22:15:24 +0000

    Mods in zeppelin

--- zeppelin/zeppelin-env.sh
+++ zeppelin/zeppelin-env.sh
@@ -1,9 +1,9 @@
 export MASTER="spark://spark-master:7077"
-export SPARK_HOME=/opt/spark
-export ZEPPELIN_JAVA_OPTS="-Dspark.jars=/opt/spark/lib/gcs-connector-latest-hadoop2.jar"
+export SPARK_HOME=/spark
+export ZEPPELIN_JAVA_OPTS="-Dspark.jars=/spark/lib/gcs-connector-latest-hadoop2.jar"
 # TODO(zmerlynn): Setting global CLASSPATH *should* be unnecessary,
 # but ZEPPELIN_JAVA_OPTS isn't enough here. :(
-export CLASSPATH="/opt/spark/lib/gcs-connector-latest-hadoop2.jar"
+export CLASSPATH="/spark/lib/gcs-connector-latest-hadoop2.jar"
 export ZEPPELIN_NOTEBOOK_DIR="${ZEPPELIN_HOME}/notebook"
 export ZEPPELIN_MEM=-Xmx1024m
 export ZEPPELIN_PORT=8080
