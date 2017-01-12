5a89bf0ac41b803732d47b15a83295f72f25ef44
dtraskas <dtraskas@gmail.com>
2017-01-12 10:06:50 +0000

    Added deployment script

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+printf "Deploying K8S cluster...\n"
+printf "Start spark-master...\n"
+kubectl create -f spark-hdfs-master-service.yaml
+kubectl create -f spark-hdfs-master-controller.yaml
+
+kubectl get pods
+while [ $(kubectl get pods | grep spark-master | grep Running | wc -l) -le 0 ]
+do
+  kubectl get pods
+  sleep 1
+done
+
+sleep 3
+
+printf "\nStart spark-worker...\n"
+kubectl create -f spark-worker-controller.yaml
+kubectl get pods
+while [ $(kubectl get pods | grep spark-worker | grep Pending | wc -l) -gt 0 ]
+do
+  kubectl get pods
+  sleep 1
+done
+
+kubectl get pods
+printf "\nCluster deployed!\n"
