fad7c45ca2dc25ac30e4613e1eec444d7a22b6d8
dtraskas <dtraskas@gmail.com>
2017-01-12 10:34:09 +0000

    Fixed bug in deployment

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ printf "Deploying K8S cluster...\n"
 kubectl create -f hdfs-spark-namespace.yaml
 
 printf "Start spark-master...\n"
-kubectl create -f spark-hdfs-master-service.yaml
-kubectl create -f spark-hdfs-master-controller.yaml
+kubectl create -f hdfs-spark-master-service.yaml
+kubectl create -f hdfs-spark-master-controller.yaml
 
 kubectl get pods
 while [ $(kubectl get pods | grep spark-master | grep Running | wc -l) -le 0 ]
