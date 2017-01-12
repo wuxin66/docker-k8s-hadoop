600245fdc8062546347bfc29514ca3da9ac98bcc
dtraskas <dtraskas@gmail.com>
2017-01-12 10:41:17 +0000

    Added shutdown script

---
+++ shutdown.sh
@@ -0,0 +1,15 @@
+printf "Shutdown cluster...\n"
+kubectl delete -f hdfs-spark-master-controller.yaml 
+kubectl delete -f spark-worker-controller.yaml 
+kubectl delete -f hdfs-spark-master-service.yaml 
+kubectl delete rc spark-driver
+
+kubectl get pods
+while [ $(kubectl get pods | grep -E 'spark-master|spark-worker|spark-driver' | grep Terminating | wc -l) -gt 0 ]
+do
+  kubectl get pods
+  sleep 1
+done
+
+kubectl get pods
+printf "\nFinished!!\n"
