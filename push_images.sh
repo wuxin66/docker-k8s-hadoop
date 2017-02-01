004b753231f6ceeebafffc2ef1ff79408ecd8ce2
dtraskas <dtraskas@gmail.com>
2017-02-01 12:48:11 +0000

    Added image deployment in google registry

---
+++ push_images.sh
@@ -0,0 +1,4 @@
+gcloud docker -- push gcr.io/caci-main/hdfs-spark-base:latest && \
+gcloud docker -- push gcr.io/caci-main/spark-master:latest && \
+gcloud docker -- push gcr.io/caci-main/spark-worker:latest && \
+gcloud docker -- push gcr.io/caci-main/zeppelin:latest
