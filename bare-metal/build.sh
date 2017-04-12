fd81f17a82743abdee03c64215c2cf0c01f9f15a
Dimitrios Traskas <dtraskas@gmail.com>
2017-04-12 11:58:56 +0100

    Changed build script for images

--- bare-metal/build.sh
+++ bare-metal/build.sh
@@ -1,4 +1,4 @@
-docker build -t gcr.io/caci-main/hdfs-spark-base hdfs-spark-base && \
-docker build -t gcr.io/caci-main/spark-master spark-master && \
-docker build -t gcr.io/caci-main/spark-worker spark-worker && \
-docker build -t gcr.io/caci-main/zeppelin zeppelin
+docker build -t cacidocker-caciuk.azurecr.io/caci-main/hdfs-spark-base hdfs-spark-base && \
+docker build -t cacidocker-caciuk.azurecr.io/caci-main/spark-master spark-master && \
+docker build -t cacidocker-caciuk.azurecr.io/caci-main/spark-worker spark-worker && \
+docker build -t cacidocker-caciuk.azurecr.io/caci-main/zeppelin zeppelin
