b1e2abec9762b38636a392c0e9b7c50fc7ab3135
Dimitrios Traskas <dtraskas@gmail.com>
2017-04-13 10:07:44 +0100

    Changed nodeports

--- bare-metal/push_images.sh
+++ bare-metal/push_images.sh
@@ -1,6 +1,6 @@
 docker login cacidocker-caciuk.azurecr.io -u <username> -p <password>
 
-docker push gcr.io/caci-main/hdfs-spark-base:latest && \
-docker push gcr.io/caci-main/spark-master:latest && \
-docker push gcr.io/caci-main/spark-worker:latest && \
-docker push gcr.io/caci-main/zeppelin:latest
+docker push cacidocker-caciuk.azurecr.io/caci-main/hdfs-spark-base:latest && \
+docker push cacidocker-caciuk.azurecr.io/caci-main/spark-master:latest && \
+docker push cacidocker-caciuk.azurecr.io/caci-main/spark-worker:latest && \
+docker push cacidocker-caciuk.azurecr.io/caci-main/zeppelin:latest
